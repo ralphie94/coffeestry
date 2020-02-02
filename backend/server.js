2186af0c4bff73809d1139f982f178932d46ea3a
Ralphie Celedon <ralphie94@pacbell.net>
2020-02-01 19:38:54 -0800

    upload image works in backend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const bodyParser = require("body-parser");
 require("dotenv").config();
 
 require("./db/db");
@@ -14,7 +15,10 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use("/uploads", express.static("uploads"));
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(logger("dev"));
 
