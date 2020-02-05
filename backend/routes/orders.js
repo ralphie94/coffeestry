8b4584358bfbbdacd24ff23c92c4dc6b45393b35
Ralphie Celedon <ralphie94@pacbell.net>
2020-02-05 14:17:13 -0800

    created get route for orders

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -6,8 +6,32 @@ const Order = require("../models/Order");
 const Coffee = require("../models/Coffee");
 
 router.get("/", (req, res, next) => {
-    
-})
+    Order.find()
+      .select("coffee quantity _id")
+      .populate('coffee', 'name')
+      .exec()
+      .then(docs => {
+        res.status(200).json({
+          count: docs.length,
+          orders: docs.map(doc => {
+            return {
+              _id: doc._id,
+              coffee: doc.coffee,
+              quantity: doc.quantity,
+              request: {
+                type: "GET",
+                url: "http://localhost:5000/orders/" + doc._id
+              }
+            };
+          })
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: err
+        });
+      });
+  });
 
 router.post("/", (req, res, next) => {
     const order = new Order({
