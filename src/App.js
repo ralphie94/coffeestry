2aae3c51e1982e5e37f12a35421641f40f1248f7
Michael Christenson <mchristenson5891@gmail.com>
2020-02-06 20:04:17 -0800

    fix register bug

--- src/App.js
+++ src/App.js
@@ -1,58 +1,76 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Navbar from "./components/Navbar/Navbar";
-import SideDrawer from "./components/SideDrawer/SideDrawer";
-import Backdrop from "./components/Backdrop/Backdrop";
-import CreateCoffee from "./components/CreateCoffee/CreateCoffee";
-import CoffeeList from "./components/CoffeeList/CoffeeList";
-import CoffeeShow from "./components/CoffeeShow/CoffeeShow";
-import Cart from "./components/Cart/Cart";
-import Login from "./components/Login/Login";
-import Register from "./components/Register/Register";
+import Navbar from './components/Navbar/Navbar';
+import SideDrawer from './components/SideDrawer/SideDrawer';
+import Backdrop from './components/Backdrop/Backdrop';
+import CreateCoffee from './components/CreateCoffee/CreateCoffee';
+import CoffeeList from './components/CoffeeList/CoffeeList';
+import CoffeeShow from './components/CoffeeShow/CoffeeShow';
+import Cart from './components/Cart/Cart';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
 
-import "./App.css";
+import './App.css';
 
 class App extends Component {
   state = {
     currentUser: null,
-    sideDrawerOpen: false
+    sideDrawerOpen: false,
   };
 
   doSetCurrentUser = user =>
     this.setState({
-      currentUser: user
-  });
+      currentUser: user,
+    });
 
   drawerTogglerClickHandler = () => {
-    this.setState((prevState) => {
-      return {sideDrawerOpen: !prevState.sideDrawerOpen};
+    this.setState(prevState => {
+      return { sideDrawerOpen: !prevState.sideDrawerOpen };
     });
   };
 
   backdropClickHandler = () => {
-    this.setState({sideDrawerOpen: false});
+    this.setState({ sideDrawerOpen: false });
   };
 
   render() {
     let backdrop;
 
     if (this.state.sideDrawerOpen) {
-      backdrop = <Backdrop click={this.backdropClickHandler} />
+      backdrop = <Backdrop click={this.backdropClickHandler} />;
     }
     return (
       <div>
         <Router>
-        <Navbar drawerClickHandler={this.drawerTogglerClickHandler} />
-        <SideDrawer show={this.state.sideDrawerOpen} click={this.backdropClickHandler} />
-        {backdrop}
-          <div className="container">
-            <Route path="/" exact component={CoffeeList} />
-            <Route path="/create" component={CreateCoffee} />
-            <Route path="/cart" component={Cart} />
-            <Route path="/coffee/:id" component={CoffeeShow} />
-            <Route path="/login" component={Login} currentUser={this.state.currentUser} doSetCurrentUser={this.doSetCurrentUser} />
-            <Route path="/register" component={Register} currentUser={this.state.currentUser} doSetCurrentUser={this.doSetCurrentUser} />
+          <Navbar
+            drawerClickHandler={this.drawerTogglerClickHandler}
+          />
+          <SideDrawer
+            show={this.state.sideDrawerOpen}
+            click={this.backdropClickHandler}
+          />
+          {backdrop}
+          <div className='container'>
+            <Route path='/' exact component={CoffeeList} />
+            <Route path='/create' component={CreateCoffee} />
+            <Route path='/cart' component={Cart} />
+            <Route path='/coffee/:id' component={CoffeeShow} />
+            <Route
+              path='/login'
+              component={Login}
+              currentUser={this.state.currentUser}
+              doSetCurrentUser={this.doSetCurrentUser}
+            />
+            <Route
+              path='/register'
+              render={() => (
+                <Register
+                  currentUser={this.state.currentUser}
+                  doSetCurrentUser={this.doSetCurrentUser}
+                />
+              )}
+            />
           </div>
         </Router>
       </div>
