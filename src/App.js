2ead8efdf334d52c0a10465c85988d0d39cf9d3e
Ralphie <ralphie94@pacbell.net>
2020-06-14 22:06:20 -0700

    merged code to fix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Navbar from './components/Navbar/Navbar';
-import SideDrawer from './components/SideDrawer/SideDrawer';
-import Backdrop from './components/Backdrop/Backdrop';
-import CreateCoffee from './components/CreateCoffee/CreateCoffee';
-import CoffeeList from './components/CoffeeList/CoffeeList';
-import CoffeeShow from './components/CoffeeShow/CoffeeShow';
-import Cart from './components/Cart/Cart';
-import Login from './components/Login/Login';
-import Register from './components/Register/Register';
+import Navbar from "./components/Navbar/Navbar";
+import SideDrawer from "./components/SideDrawer/SideDrawer";
+import Backdrop from "./components/Backdrop/Backdrop";
+import CreateCoffee from "./components/CreateCoffee/CreateCoffee";
+import CoffeeList from "./components/CoffeeList/CoffeeList";
+import CoffeeShow from "./components/CoffeeShow/CoffeeShow";
+import Cart from "./components/Cart/Cart";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   state = {
@@ -21,46 +21,44 @@ class App extends Component {
 
   doSetCurrentUser = user =>
     this.setState({
-      currentUser: user,
-    });
+      currentUser: user
+  });
 
   drawerTogglerClickHandler = () => {
-    this.setState(prevState => {
-      return { sideDrawerOpen: !prevState.sideDrawerOpen };
+    this.setState((prevState) => {
+      return {sideDrawerOpen: !prevState.sideDrawerOpen};
     });
   };
 
   backdropClickHandler = () => {
-    this.setState({ sideDrawerOpen: false });
+    this.setState({sideDrawerOpen: false});
   };
 
   render() {
     let backdrop;
 
     if (this.state.sideDrawerOpen) {
-      backdrop = <Backdrop click={this.backdropClickHandler} />;
+      backdrop = <Backdrop click={this.backdropClickHandler} />
     }
     return (
       <div>
         <Router>
-          <Navbar
-            drawerClickHandler={this.drawerTogglerClickHandler}
-          />
-          <SideDrawer
-            show={this.state.sideDrawerOpen}
-            click={this.backdropClickHandler}
-          />
-          {backdrop}
-          <div className='container'>
-            <Route path='/' exact component={CoffeeList} />
-            <Route path='/create' component={CreateCoffee} />
-            <Route path='/cart' component={Cart} />
-            <Route path='/coffee/:id' component={CoffeeShow} />
+        <Navbar drawerClickHandler={this.drawerTogglerClickHandler} currentUser={this.state.currentUser} />
+        <SideDrawer show={this.state.sideDrawerOpen} click={this.backdropClickHandler} currentUser={this.state.currentUser} />
+        {backdrop}
+          <div className="container">
+            <Route path="/" exact component={CoffeeList} />
+            <Route path="/create" component={CreateCoffee} />
+            <Route path="/cart" render={() => <Cart currentUser={this.state.currentUser} />} />
+            <Route path="/coffee/:id" render={() => (<CoffeeShow currentUser={this.state.currentUser} />)}/>
             <Route
               path='/login'
-              component={Login}
-              currentUser={this.state.currentUser}
-              doSetCurrentUser={this.doSetCurrentUser}
+              render={() => (
+                <Login
+                  currentUser={this.state.currentUser}
+                  doSetCurrentUser={this.doSetCurrentUser}
+                />
+              )}
             />
             <Route
               path='/register'
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,106 +1,70 @@
-import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
 
 import "./Register.css";
 
 class Register extends Component {
-  state = {
-    username: '',
-    password: '',
-    email: '',
-    logged: false,
-    message: '',
-  };
-
-  changeHandler = e => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  handleSubmit = async e => {
-    e.preventDefault();
-    const loginResponse = await fetch('/users', {
-      method: 'POST',
-      credentials: 'include',
-      body: JSON.stringify(this.state),
-      headers: {
-        'Content-type': 'application/json',
-      },
-    });
-    const parsedResponse = await loginResponse.json();
-    if (parsedResponse.success) {
-      console.log('HIT FRONT');
-      this.props.doSetCurrentUser(parsedResponse.user);
-      this.setState({
-        logged: true,
-      });
-    } else {
-      console.log('HIT ELSE');
-      this.setState({
-        message: 'Try again!',
-      });
+    state = {
+        username: "",
+        password: "",
+        email: "",
+        logged: false,
+        message: ""
     }
-  };
 
-  render() {
-    const { username, password, email } = this.state;
-    return (
-      <div>
-        {this.state.logged ? (
-          <Redirect to={`/`} />
-        ) : (
-            <RegisterForm
-              changeHandler={this.changeHandler}
-              handleSubmit={this.handleSubmit}
-              username={username}
-              password={password}
-              email={email}
-            />
-          )}
-      </div>
-    );
-  }
+    changeHandler = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) =>{
+        e.preventDefault()
+        fetch ("/users", {
+            method: "POST",
+            credentials: "include",
+            body: JSON.stringify(this.state),
+            headers:{
+                "Content-type" : 'application/json'
+            }
+        })
+        .then(results => results.json())
+        .then(data => this.setState({data: data, logged: true}))
+    }
+
+    render(){
+        const  { username, password, email } = this.state
+        return (
+            <div>
+                {
+                    this.state.logged
+                    ? <Redirect to={`/`}/>
+                    : <RegisterForm 
+                    changeHandler={this.changeHandler} 
+                    handleSubmit={this.handleSubmit} 
+                    username={username} 
+                    password={password} 
+                    email={email}/>
+                }
+            </div>
+           
+        )
+    }
 }
-const RegisterForm = ({
-  changeHandler,
-  handleSubmit,
-  username,
-  password,
-  email,
-}) => (
-    <div>
-      <form onSubmit={e => handleSubmit(e)}>
-        <h1>Register</h1>
-        <input
-          onChange={changeHandler}
-          type='text'
-          name='username'
-          placeholder='Username'
-          value={username}
-        ></input>
-        <br />
-        <input
-          onChange={changeHandler}
-          type='password'
-          name='password'
-          placeholder='Password'
-          value={password}
-        ></input>
-        <br />
-        <input
-          onChange={changeHandler}
-          type='text'
-          name='email'
-          placeholder='Email'
-          value={email}
-        ></input>
-        <br />
-        <button type='submit' value='Submit'>
-          Submit
-      </button>
-      </form>
+const RegisterForm = ({changeHandler, handleSubmit, username, password, email}) =>
+<div>
+<form onSubmit={e => handleSubmit(e)}>
+    <div className="login-container">
+        <h1 className="register-title">Register</h1>
+        <h2 className="username">Username</h2>
+        <input onChange={changeHandler} type="text" className="name-register" name="username" value={username}></input><br/>
+        <h2 className="password">Password</h2>
+        <input onChange={changeHandler} type="password" className="password-register" name="password" value={password}></input><br/>
+        <h2 className="email">Email</h2>
+        <input onChange={changeHandler} type="text" className="email-box" name="email" value={email}></input><br/>
+        <button type="submit" className="register-btn" value="Submit">CREATE</button>
     </div>
-  );
+</form>
+</div>
 
 export default Register;
